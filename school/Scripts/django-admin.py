1b226aa1b6b095325c59d7fb2000336571c74d11
Onyenaucheya Emmanuel <94042276+OnyenaucheyaEmmanuel@users.noreply.github.com>
2022-12-01 16:21:24 +0100

    Add files via upload

---
+++ school/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\projects\schoolmanagement-master\schoolmanagement\school\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
